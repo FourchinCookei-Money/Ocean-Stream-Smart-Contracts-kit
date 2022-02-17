84724fc51c34a8a643dbc12ac5c80318b4c33bfe
Fourchin Cookei Money <94145203+FourchinCookei-Money@users.noreply.github.com>
2022-02-17 15:53:42 -0800

    Add files via upload

---
+++ ERC223CustomFallback.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/FourchinCookei-Money/IERC223.sol";
+import "https://github.com/FourchinCookei-Money/ERC223CustomFallback.sol";
+import "https://github.com/FourchinCookei-Money/ERC223.sol";
+
+/**
+ * @title Reference implementation of the ERC223 standard token with custom fallback invocations.
+ */
+abstract contract ERC223CustomFallbackToken is ERC223Token {
+    
+    event Response(bool _success, bytes _data);
+    
+    function transfer(address _to, uint _value, bytes calldata _data, string calldata _custom_fallback) public returns (bool success)
+    {
+        // Standard function transfer similar to ERC20 transfer with no _data .
+        // Added due to backwards compatibility reasons .
+        balances[msg.sender] = balances[msg.sender] - _value;
+        balances[_to] = balances[_to] + _value;
+        if(Address.isContract(_to)) {
+            (bool _success, bytes memory _response) = address(_to).call(abi.encodeWithSignature(_custom_fallback, msg.sender, _value, _data));
+            require(_success, "Fallback function execution failed on receivers side");
+            
+            emit Response(_success, _response);
+        }
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
